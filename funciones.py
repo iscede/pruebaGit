64985cccb17f5c2aee674e2fa72afe7781a58c93
andre <andreisrael11@gmail.com>
2022-06-22 19:57:32 -0500

    Inicio de Proyecto

---
+++ funciones.py
@@ -0,0 +1,31 @@
+import math
+
+def primo(num):
+    if num < 0:
+        return 'No se permiten números negativos.'
+
+    if num <= 1:
+        return False
+
+    if num == 2:
+        return True
+
+    if num % 2 == 0:
+        return False
+
+    for n in range(2, num):
+        if num % n == 0:
+            print("No es primo", n, "es divisor")
+            return False
+    print("Es primo")
+    return True
+
+
+
+def cubica(a):
+    return a * a * a
+
+
+def saludo(name):
+    return "Hola, " + name
+
---
+++ test_funciones.py
@@ -0,0 +1,31 @@
+import unittest
+from funciones import primo, cubica, saludo
+
+
+class Testfunciones(unittest.TestCase):
+    def test_primo(self):
+        self.assertIn("No se permiten números negativos.", primo(-10)) #if num<0
+       
+        self.assertFalse(primo(0), 1) #if num<=1
+
+        self.assertTrue(primo(2),2) #if num==2
+
+        self.assertEqual(primo(8),0) # if num%2 
+
+        self.assertEqual(primo(7),1) #for n
+
+    def test_cubica(self):
+        self.assertEqual(cubica(0),0)
+        self.assertEqual(cubica(1),1)
+        self.assertEqual(cubica(-3), -27)
+    
+    def test_saludo(self):
+        self.assertEqual(saludo("Israel"), "Hola, Israel")
+        self.assertEqual(saludo("Francisco"), "Hola, Francisco")
+        self.assertIn(saludo("Israel"), "Hola, Israel")
+        self.assertIn(saludo("Francisco"), "Hola, Israel")
+
+
+
+if __name__=="__main__":
+    unittest.main()
